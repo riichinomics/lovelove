1891e30e05c8fbbc46f6d1c04d5a3e77aea75f16
hierarch <riichinomics@gmail.com>
2020-08-07 15:29:06 +1200

    Added grpc connection to websocket on server

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,9 @@
+fetch('http://localhost:6482/proto/muhjong.proto')
+    .then(response => response.text())
+    .then(data => console.log(data));
+
+
+
 const webSocket = new WebSocket("ws://localhost:6482/echo");
 webSocket.onopen = function (event) {
     console.log(event);
--- server/server.go
+++ server/server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"google.golang.org/grpc"
+	muhjong "riichi.moe/muhjong/proto"
 )
 
 var addr = flag.String("addr", "localhost:6482", "http service address")
@@ -38,12 +44,98 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type MuhjongRpcServer struct {
+	muhjong.UnimplementedMuhjongServer
+}
+
+func (MuhjongRpcServer) SayHello(context context.Context, request *muhjong.HelloRequest) (*muhjong.HelloReply, error) {
+	log.Print(request.Name)
+	return &muhjong.HelloReply{Message: "Hello " + request.Name}, nil
+}
+
+type WebSocketListener struct {
+	websocketOpened chan *websocket.Conn
+}
+
+type WebSocketConn struct {
+	*websocket.Conn
+	reader io.Reader
+}
+
+func NewWebSocketListener() *WebSocketListener {
+	return &WebSocketListener{
+		websocketOpened: make(chan *websocket.Conn),
+	}
+}
+
+func (conn *WebSocketConn) Read(buffer []byte) (int, error) {
+	return conn.reader.Read(buffer)
+}
+
+func (conn *WebSocketConn) Write(buffer []byte) (int, error) {
+	err := conn.WriteMessage(websocket.BinaryMessage, buffer)
+	if err != nil {
+		return 0, err
+	}
+	return len(buffer), nil
+}
+
+func (conn *WebSocketConn) SetDeadline(t time.Time) error {
+	err := conn.SetReadDeadline(t)
+	if err != nil {
+		return err
+	}
+	return conn.SetWriteDeadline(t)
+}
+
+func (listener WebSocketListener) Accept() (net.Conn, error) {
+	conn := <-listener.websocketOpened
+	return &WebSocketConn{
+		Conn:   conn,
+		reader: websocket.JoinMessages(conn, ""),
+	}, nil
+}
+
+func (WebSocketListener) Close() error {
+	return nil
+}
+
+func (WebSocketListener) Addr() net.Addr {
+	ifaces, _ := net.Interfaces()
+	// handle err
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		// handle err
+		for _, addr := range addrs {
+			return addr
+		}
+	}
+	return nil
+}
+
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	http.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path)
+
+	listener := NewWebSocketListener()
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade:", err)
+			return
+		}
+		listener.websocketOpened <- c
 	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
 	log.Fatal(http.ListenAndServe(*addr, nil))
+
+	server := grpc.NewServer()
+	muhjong.RegisterMuhjongServer(server, &MuhjongRpcServer{})
+	server.Serve(listener)
 }
