211d961943fa1649fa2b6dc118727cf7f2bf90c5
hierarch <riichinomics@gmail.com>
2021-11-06 23:52:17 +1300

    Create unique user ID on first client launch

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,6 +12,15 @@ import { ThemeContext } from "./themes/ThemeContext";
 import mainReducer from "./state/reducers";
 import mantia from "./themes/mantia";
 import { ApiState } from "./rpc/ApiState";
+import * as uuid from "uuid";
+
+const USER_ID_LOCAL_STORAGE_KEY = "lovelove_user_id";
+const storedUserId = localStorage.getItem(USER_ID_LOCAL_STORAGE_KEY);
+const userId = storedUserId ?? uuid.v4();
+
+if (!storedUserId) {
+	localStorage.setItem(USER_ID_LOCAL_STORAGE_KEY, userId);
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -19,6 +28,7 @@ const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 const store = createStore(
 	mainReducer,
 	{
+		userId,
 		apiState: ApiState.Connecting
 	} as IState,
 	composeEnhancers()
@@ -27,7 +37,7 @@ const store = createStore(
 const api = new Api({url: "localhost:6482"});
 api.init().then(() => {
 	api.lovelove.authenticate({
-		userId: "test",
+		userId,
 	}).then((response) => {
 		console.log("authentication response", response);
 		store.dispatch<ApiStateChangedAction>({
--- client/src/state/IState.ts
+++ client/src/state/IState.ts
@@ -2,6 +2,7 @@ import { ApiState } from "../rpc/ApiState";
 import { lovelove } from "../rpc/proto/lovelove";
 
 export interface IState {
+	userId: string;
 	apiState: ApiState;
 	gameState?: lovelove.ICompleteGameState;
 }
